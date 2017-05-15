83f4a35279b19398053b9529cbf636548a7bd3e0
Michal Boleslav Měchura <valselob@gmail.com>
2017-05-15 15:43:38 +0200

    add Czech localization

---
+++ screenful-loc-cs.js
@@ -0,0 +1,26 @@
+Screenful.Loc={
+  howmany: function(n){
+	if(n==1) return n+" heslo";
+	if(n>1 && n<5) return n+" hesla";
+	return n+" hesel";
+  },
+  ready: "Hotovo.",
+  filter: "Filtrovat",
+  removeFilter: "Odstranit filtr",
+  reload: "Obnovit",
+  cancel: "Storno",
+  more: "Víc",
+  new: "Nové",
+  edit: "Upravit",
+  save: "Uložit",
+  delete: "Smazat",
+  listing: "Čekáme na seznam hesel ze serveru...",
+  listingFailed: "Nepovedlo se získat seznam hesel ze serveru.",
+  reading: "Čekáme na heslo ze serveru...",
+  readingFailed: "Žádné takové heslo neexistuje.",
+  saving: "Ukládá se...",
+  savingFailed: "Nepovedlo se uložit heslo.",
+  deleting: "Maže se heslo...",
+  deletingFailed: "Nepovedlo se smazat heslo.",
+  deleteConfirm: "Pozor s tímhle! Opravdu to chceš smazat?",
+};
