564b51371920aaab34f7e0be1399e42375bb7961
Michal Měchura <michmech@lexiconista.com>
2023-04-29 17:49:24 +0200

    optionally allow scrolling in curtain pop-up

--- screenful-curtain.js
+++ screenful-curtain.js
@@ -12,4 +12,17 @@ Screenful.Curtain={
       callback();
     });
   },
+  openScrollable: function(url, callback){
+    $("#curtainframe").remove();
+    var $curtain=$(`
+      <div id="curtainframe">
+        <div class="shade"></div>
+        <div class="inside"><div class="margin"><iframe src="${url}" scrolling="yes" frameborder="0" /></div></div>
+      </div>
+    `).appendTo("body").hide().fadeIn("slow");
+    $curtain.on("click", function(){
+      $("#curtainframe").remove();
+      callback();
+    });
+  },
 };
